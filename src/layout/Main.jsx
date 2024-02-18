ff9cf7ad86d4fc3f64988a7ef38b98ce402340eb
Mahbub2027 <alam201127@gmail.com>
2024-02-19 01:35:11 +0600

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+import { Outlet } from "react-router-dom";
+import Navbar from "../sharedComponents/Navbar";
+import Footer from "../sharedComponents/Footer";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/sharedComponents/Footer.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer items-center justify-center p-4 bg-neutral text-neutral-content">
+               
+                    <p>Copyright © 2024 Md. Mahbub Alam || All right reserved</p>
+           
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
